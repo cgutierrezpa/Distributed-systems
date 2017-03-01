b5a56e28b220ec3040b46aa520ea50f46f430233
Guti <100291121@alumnos.uc3m.es>
2017-03-01 09:40:51 +0100

    CGP: Changes responses.h for protocol.h. Updated client.c

--- ex1/client.c
+++ ex1/client.c
@@ -6,31 +6,34 @@
 #include <time.h>
 #include <unistd.h>
 #include <mqueue.h>
-#include "responses.h"
+#include <string.h>
+#include "protocol.h"
 
-//#define MAX_BUFFER		1024
-#define NUM_MSGS		1
-
-int main(int argc, char *argv[]){
+int main(void){
 	mqd_t clientq;
 	mqd_t serverq;
 	struct mq_attr att;
 	struct request req;
 	struct response res;
 
+	/* Initialize request parameters */
 	req.name = "vector1";
-	//sprintf(req.queue, "vector1");
-	req.Ni = 20;
-	req.op = 0;
+	req.Ni = 20;			//N
+	req.op = 0;				//Init
+	req.queue = "/CLIENT1";
+
+	/* Set queue attributes */
 	att.mq_maxmsg = NUM_MSGS;
 	att.mq_msgsize = sizeof(struct request);
-	//sprintf(req.queue, "/CLIENT1");
-	//clientq = mq_open("/CLIENT1", O_RDONLY|O_CREAT, 777, &att);
+	
+	/* Open the queue */
+	if((clientq = mq_open(req.queue, O_RDONLY|O_CREAT, 0777, &att)) == -1){
+		perror("Error when opening the queue\n");
+		exit(-1);
+	}
 
-	printf("%s\n", req.name);
-	printf("%d\n", req.Ni);
-	printf("%d\n", req.op);
 
-	//mq_close(clientq);
-	return 1;
+	mq_close(clientq);
+	
+	exit(0);
 }
--- ex1/protocol.h
+++ ex1/protocol.h
@@ -1,14 +1,13 @@
-#ifndef RESPONSES_H
-#define RESPONSES_H
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+#define NUM_MSGS		1
 
 struct request{
-	//char name[MAX_BUFFER];		/* For the name of the array */
-	char *name;		/* For the name of the array */
+	char *name;					/* For the name of the array */
 	int Ni;						/* To indicate the value of N in case of calling init, or 'i' in case of calling set or get */
 	int value;					/* For the input parameter of set and get */
 	char op;					/* To indicate the operation that we are calling. 0: init; 1: set; 2: get; 3: destroy*/
-	//char queue[MAX_BUFFER];		/* Queue name for the server to respond */
-	char *queue;		/* Queue name for the server to respond */
+	char *queue;				/* Queue name for the server to respond */
 };
 
 struct response{
